883624cb8fb832a2e60062e335cd930eff0be48d
Pam Willenz <pam@positek.net>
2016-08-11 14:11:50 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectQuiz::Application.config.secret_key_base = 'b738a0465bee7b44e6ae064c8e56ce5130a2a94f66e24f2f4b9d972fe6639faafb73b1dbcab5717c6e4d3aa0ccaf36bb1e421810b3afd1baff5d27236e31c3a3'
